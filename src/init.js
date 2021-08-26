1ffd7491bddb55e2e73d57c33c18f0dd617959f4
dongjin <ehdwlsdlwkd@naver.com>
2021-08-26 17:14:46 +0900

    add

--- src/init.js
+++ src/init.js
@@ -9,6 +9,6 @@ import app from "./server";
 const PORT = process.env.PORT || 4000; // heroku가 주는 PORT있으면 그걸로, 없으면 내 컴퓨터 PORT로
 
 const handleListening = () => 
-    console.log(`Server listening on port http://localhost${PORT}`); // handler 정의
+    console.log(`Server listening on port http://localhost:${PORT}`); // handler 정의
 
 app.listen(4000, handleListening); // start server listening / port 번호와 서버가 시작될 때 작동하는 함수를 전달
